1952b3955ebc1395678ba7033447d53198198887
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-18 22:44:58 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vehicle_registration_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
