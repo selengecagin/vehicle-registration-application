d084db215e7fa6754a6a77dfb8da5eb2fbc36eb0
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 08:15:21 +0300

    brand repo class implemented

--- Repositories/BrandRepo.cs
+++ Repositories/BrandRepo.cs
@@ -1,6 +1,17 @@
-﻿namespace vehicle_registration_app.Repositories
+﻿using vehicle_registration_app.AppDB;
+using vehicle_registration_app.Models;
+
+namespace vehicle_registration_app.Repositories
 {
-    public class BrandRepo
+    public class BrandRepo : IBrandRepository
     {
+        private readonly ApplicationDbContext? _context;
+
+        public IEnumerable<Brand> GetAll()
+        {
+            return _context.Brands.ToList();
+
+
+
+        }
     }
-}
