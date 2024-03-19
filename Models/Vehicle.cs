e4efe69744382c0a3eb21a6fb9ebe25ffa034e37
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 07:35:03 +0300

    vehicle model created

---
+++ Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace vehicle_registration_app.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string CarPlate { get; set; }
+        public int Km { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string GearType { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
