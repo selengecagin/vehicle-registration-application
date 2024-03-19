8c2a60332967d643062b5a0cc58dfa2eeec17ee5
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 09:00:04 +0300

    vehicle service created

--- Services/VehicleService.cs
+++ Services/VehicleService.cs
@@ -1,8 +1,41 @@
 ﻿using System.Collections.Generic;
+using vehicle_registration_app.Models;
+using vehicle_registration_app.Repositories;
 namespace vehicle_registration_app.Services
 
 {
     public class VehicleService
     {
+        private readonly IVehicleRepository _vehicleRepository;
+
+        public VehicleService(IVehicleRepository vehicleRepository)
+        {
+            _vehicleRepository = vehicleRepository;
+        }
+
+        public IEnumerable<Vehicle> GetAllVehicles()
+        {
+            return _vehicleRepository.GetAll();
+        }
+
+        public Vehicle GetVehicleById(int id)
+        {
+            return _vehicleRepository.GetById(id);
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            _vehicleRepository.Add(vehicle);
+        }
+
+        public void UpdateVehicle(Vehicle vehicle)
+        {
+            _vehicleRepository.Update(vehicle);
+        }
+
+        public void DeleteVehicle(int id)
+        {
+            _vehicleRepository.Delete(id);
+        }
     }
 }
