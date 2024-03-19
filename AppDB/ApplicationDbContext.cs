6a02f12fabe8baf0c19f7de5c95ca8a421cd354f
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 08:24:07 +0300

    app db context error handling

--- AppDB/ApplicationDbContext.cs
+++ AppDB/ApplicationDbContext.cs
@@ -5,13 +5,13 @@ using vehicle_registration_app.Models;
 
 namespace vehicle_registration_app.AppDB
 {
-    public class ApplicationDbContext : System.Data.Entity.DbContext
+    public class ApplicationDbContext :DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
-
+        // DbSet properties and configurations
         public DbSet<Brand>? Brands { get; set; }
         public DbSet<Vehicle>? Vehicles { get; set; }
     }
--- Repositories/BrandRepo.cs
+++ Repositories/BrandRepo.cs
@@ -1,17 +1,40 @@
-﻿using vehicle_registration_app.AppDB;
+﻿using Microsoft.EntityFrameworkCore;
+using vehicle_registration_app.AppDB;
 using vehicle_registration_app.Models;
 
 namespace vehicle_registration_app.Repositories
 {
-    public class BrandRepo : IBrandRepository
+    public class BrandRepo(ApplicationDbContext context) : IBrandRepository
     {
-        private readonly ApplicationDbContext? _context;
+        private readonly ApplicationDbContext? _context = context;
 
         public IEnumerable<Brand> GetAll()
         {
             return _context.Brands.ToList();
-
-
-
         }
+
+        public Brand GetById(int id)
+        {
+            return _context.Brands.Find(id);
+        }
+
+        public void Add(Brand brand)
+        {
+            _context.Brands.Add(brand);
+            _context.SaveChanges();
+        }
+
+        public void Update(Brand brand)
+        {
+            _context.Entry(brand).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var brand = _context.Brands.Find(id);
+            _context.Brands.Remove(brand);
+            _context.SaveChanges();
+        }
+
     }
