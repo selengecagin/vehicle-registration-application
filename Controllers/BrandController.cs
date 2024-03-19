72ae1b46bbbaa4a8e65875f73c6acf618bc93811
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 09:15:28 +0300

    brand and vehicle controllers are created

--- Controllers/BrandController.cs
+++ Controllers/BrandController.cs
@@ -1,15 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using vehicle_registration_app.Models;
 using vehicle_registration_app.Services;
 namespace vehicle_registration_app.Controllers
 {
-    public class BrandController
+    public class BrandController : Controller
     {
         private readonly BrandService _brandService;
 
         public BrandController(BrandService brandService)
         {
             _brandService = brandService;
+        }
+
+        public IActionResult Index()
+        {
+            var brands = _brandService.GetAllBrands();
+            return View(brands);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Create(Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                _brandService.AddBrand(brand);
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var brand = _brandService.GetBrandById(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return View(brand);
+        }
+        public IActionResult Edit(Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                _brandService.UpdateBrand(brand);
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _brandService.DeleteBrand(id);
+            return RedirectToAction("Index");
         }
     }
 }
--- Controllers/VehicleController.cs
+++ Controllers/VehicleController.cs
@@ -1,6 +1,72 @@
-﻿namespace vehicle_registration_app.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using vehicle_registration_app.Models;
+using vehicle_registration_app.Services;
+namespace vehicle_registration_app.Controllers
 {
-    public class VehicleController
+    public class VehicleController : Controller
     {
+        private readonly VehicleService _vehicleService;
+        private readonly BrandService _brandService;
+
+        public VehicleController(VehicleService vehicleService, BrandService brandService)
+        {
+            _vehicleService = vehicleService;
+            _brandService = brandService;
+        }
+
+
+        public IActionResult Index()
+        {
+            var vehicles = _vehicleService.GetAllVehicles();
+            return View(vehicles);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.Brands = _brandService.GetAllBrands().Select(b => new SelectListItem { Value = b.Id.ToString(), Text = b.Name });
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _vehicleService.AddVehicle(vehicle);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Brands = _brandService.GetAllBrands().Select(b => new SelectListItem { Value = b.Id.ToString(), Text = b.Name });
+            return View(vehicle);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var vehicle = _vehicleService.GetVehicleById(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Brands = _brandService.GetAllBrands().Select(b => new SelectListItem { Value = b.Id.ToString(), Text = b.Name });
+            return View(vehicle);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _vehicleService.UpdateVehicle(vehicle);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Brands = _brandService.GetAllBrands().Select(b => new SelectListItem { Value = b.Id.ToString(), Text = b.Name });
+            return View(vehicle);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _vehicleService.DeleteVehicle(id);
+            return RedirectToAction("Index");
+        }
+
     }
 }
