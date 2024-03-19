9983c763e4915c15f66ca61dec025569d7f61e9b
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 08:10:08 +0300

    application Db Context created

--- AppDB/ApplicationDbContext.cs
+++ AppDB/ApplicationDbContext.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 using vehicle_registration_app.Models;
 
 namespace vehicle_registration_app.AppDB
 {
-    public class ApplicationDbContext { }
+    public class ApplicationDbContext : System.Data.Entity.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
 
 }
