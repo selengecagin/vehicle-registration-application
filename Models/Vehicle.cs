a033da390dafdd1ecc61a54070a6f91530c00019
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 07:36:37 +0300

    nullable ref types error fixed

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -3,13 +3,13 @@
     public class Vehicle
     {
         public int Id { get; set; }
-        public string CarPlate { get; set; }
+        public string? CarPlate { get; set; }
         public int Km { get; set; }
-        public string Model { get; set; }
-        public string Color { get; set; }
-        public string GearType { get; set; }
+        public string? Model { get; set; }
+        public string? Color { get; set; }
+        public string? GearType { get; set; }
 
         public int BrandId { get; set; }
-        public Brand Brand { get; set; }
+        public Brand? Brand { get; set; }
     }
 }
