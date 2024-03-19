ee0f0be91bc77015bb3ef69288e2c83b7e35e7b1
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 08:45:17 +0300

    vehicle repo getAll method created

--- Repositories/VehicleRepo.cs
+++ Repositories/VehicleRepo.cs
@@ -1,6 +1,23 @@
-﻿namespace vehicle_registration_app.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using vehicle_registration_app.AppDB;
+using vehicle_registration_app.Models;
+
+namespace vehicle_registration_app.Repositories
 {
-    public class VehicleRepo
+    public class VehicleRepo(ApplicationDbContext context) : IVehicleRepository
     {
+        private readonly ApplicationDbContext? _context = context;
+
+        public IEnumerable<Vehicle> GetAll()
+        {
+            if (_context != null && _context.Vehicles != null)
+            {
+                return _context.Vehicles.Include(v => v.Brand).ToList();
+            }
+            else
+            {
+                throw new Exception("Context or Vehicles collection is null.");
+            }
+        }
     }
 }
