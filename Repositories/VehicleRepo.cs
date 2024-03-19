7f928b6f6e9107f7426a13c4b21199a4101fe2a5
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 08:54:27 +0300

    vehicle repo add, update,delete methods created

--- Repositories/VehicleRepo.cs
+++ Repositories/VehicleRepo.cs
@@ -19,6 +19,40 @@ namespace vehicle_registration_app.Repositories
                    ?? throw new Exception("Vehicle not found.");
         }
 
+        public void Add(Vehicle vehicle)
+        {
+            _context?.Vehicles?.Add(vehicle);
+            _context?.SaveChanges();
+        }
+
+        public void Update(Vehicle vehicle)
+        {
+            var entry = _context?.Entry(vehicle);
+            if (entry != null)
+            {
+                entry.State = EntityState.Modified;
+                _context?.SaveChanges();
+            }
+        }
+
+
+        public void Delete(int id)
+        {
+            var vehicle = _context?.Vehicles?.Find(id);
+            if (vehicle != null)
+            {
+                _context?.Vehicles?.Remove(vehicle);
+                _context?.SaveChanges();
+            }
+            else
+            {
+                // Optionally handle the case where the vehicle with the specified ID is not found
+                throw new Exception($"Vehicle with ID {id} not found.");
+            }
+        }
+
+
+
 
     }
 }
