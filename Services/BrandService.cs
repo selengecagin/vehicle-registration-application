3a4df4b558d95aeb35780fcfd009795332862694
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-03-19 08:59:29 +0300

    brand service created

---
+++ Services/BrandService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using vehicle_registration_app.Models;
+using vehicle_registration_app.Repositories;
+namespace vehicle_registration_app.Services
+{
+    public class BrandService
+    {
+        private readonly IBrandRepository _brandRepository;
+
+        public BrandService(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+
+        public IEnumerable<Brand> GetAllBrands()
+        {
+            return _brandRepository.GetAll();
+        }
+
+        public Brand GetBrandById(int id)
+        {
+            return _brandRepository.GetById(id);
+        }
+
+        public void AddBrand(Brand brand)
+        {
+            _brandRepository.Add(brand);
+        }
+
+        public void UpdateBrand(Brand brand)
+        {
+            _brandRepository.Update(brand);
+        }
+
+        public void DeleteBrand(int id)
+        {
+            _brandRepository.Delete(id);
+        }
+    }
+}
---
+++ Services/VehicleService.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+namespace vehicle_registration_app.Services
+
+{
+    public class VehicleService
+    {
+    }
+}
